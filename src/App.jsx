e568fd2d4a342affba73c26ad1a3dacc917b2802
nigelwentworth33-art <nigelwentworth33@gmail.com>
2026-02-19 11:40:58 +0200

    polyscreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Infoscreen from './poly.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+ 
 
   return (
     <>
-      
-      <h1>Vite + React</h1>
-      
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+      <Infoscreen/>
+      </div>
+
     </>
   )
 }
 
-export default App
+
